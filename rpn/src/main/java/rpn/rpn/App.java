fac041e69c1b007403c2197387bbfcb87201799f
Utilisateur <Utilisateur@GaeBolga>
2017-01-26 16:39:56 +0100

    euler ajout de la fonction du gamma d'euler ajout d'exceptions : div 0, sqrt negative, expression incorrecte

--- rpn/src/main/java/rpn/rpn/App.java
+++ rpn/src/main/java/rpn/rpn/App.java
@@ -16,7 +16,7 @@ public class App
 
 
 	private void evaluer(String s) {
-		String tab[] = s.split(" ");
+		String tab[] = s.split(" +");
 		
 		for(int i = 0; i < tab.length; i++) {
 			String elem = tab[i];
@@ -42,10 +42,23 @@ public class App
 				else if(elem.equals("/")) {
 					Double d1 = stack.pop();
 					Double d2 = stack.pop();
+					if(d1 == 0) {
+						System.out.println("Erreur : Division par 0.");
+						System.out.println("Opérateur : [" + elem + "]");
+						System.out.println("Position : [" + (i+1) + "]");
+						return;
+					}
+						
 					stack.push(d2 / d1);
 				}
 				else if(elem.equals("sqrt")) {
 					Double d1 = stack.pop();
+					if(d1 < 0) {
+						System.out.println("Erreur : Tentative de calcul de la racine carrée d'un nombre négatif.");
+						System.out.println("Opérateur : [" + elem + "]");
+						System.out.println("Position : [" + (i+1) + "]");
+						return;
+					}
 					stack.push(Math.sqrt(d1));
 				}
 				else if(elem.equals("carré")) {
@@ -78,7 +91,8 @@ public class App
 					stack.push(Math.pow(d1, d2));
 				}
 				else if(elem.equals("!")) {
-					
+					Double d1 = stack.pop();
+					stack.push(euler(d1));
 				}
 				else {
 					stack.push(Double.parseDouble(elem));
@@ -86,8 +100,22 @@ public class App
 			}
 			catch(java.util.EmptyStackException e) {
 				System.out.println("Erreur : l'expression passée en paramètre est incorrecte; un opérateur n'a pas ou pas assez d'opérandes.");
+				System.out.println("Opérateur : [" + elem + "]");
+				System.out.println("Position : [" + (i+1) + "]");
+				return;
 			}
-
+			catch(java.lang.NumberFormatException e) {
+				System.out.println("Une erreur est survenue lors de la lecture de l'expression. Le programme a tenté de continuer l'évaluation en ignorant l'item erroné.");
+				System.out.println("Item ignoré : [" + elem + "]");
+				System.out.println("Position : [" + (i+1) + "]");
+			}
+		}
+		
+		
+		if(stack.size() != 1) {
+			System.out.println("Erreur : La pile contient plus d'un élément après évaluation de l'expression. L'expression doit être mauvaise.");
+			System.out.println("Etat de la pile : " + stack);
+			return;
 		}
 		System.out.println("Résultat : " + stack.peek());
 
@@ -97,21 +125,33 @@ public class App
 		return stack.peek();
 	}
 	
-	public static void main( String[] args )
-	{
+	public Double euler(Double x) {
+		
+			
+		Double a, b, c, d = 0.0;
+		for(int i = 0; i < 10000; i++) {
+			a = Math.exp(-i);
+			b = Math.pow(i, x-1);
+			c = a*b;
+			d += c;
+		}
+		/* Si l'opérande est un entier, le résultat est arrondi */
+		if(x == Math.round(x)) {
+			return (double) Math.round(d);
+		}
+		return d;
+	}
+	
+	public static void main( String[] args ) {
 		/*if(args.length == 0) {
 			new App();
 		}
 		else if (args.length == 1) {*/
 				new App("3 4 sqrt + 10 3 - *");
+				new App("5 !");
 		/*}
 		else {
 			System.out.println("Erreur. Le programme doit être appellé avec 0 ou 1 paramètre.");
 		}*/
-				
-		for(Operateur o : Operateur.values()) {
-			System.out.println(o.getSymbole());
-			double i = o.ADDITION.calc();
-		}	
 	}
 }
